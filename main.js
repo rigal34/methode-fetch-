2543dc845692ebe7d2afd0bc510e606498c0bc9c
ALBATOR <SST.plomberie@laposte.net>
2022-02-11 17:36:24 +0100

    Ajout d'une nouvelle requete

--- main.js
+++ main.js
@@ -10,4 +10,11 @@ usersData.then((utilisateur)=> {
     console.log(utilisateur[1].address.geo["lat"]);
 });
 })
-.catch((err)=> console.log(err));//la methode .catch il renvois l'erreur s'il y a une erreur l'argument en call back dans le log s'il y a une erreur
+.catch((err)=> console.log(err));//la methode .catch il renvois l'erreur s'il y a une erreur l'argument en call back dans le log s'il y a une erreurpr
+
+//1er requête
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then((bruno) => bruno.json())//CONVERTIT au format json pour aller dans mon body
+.then((json) => console.log(json));//affiche Object { userId: 1, id: 1, title: "delectus aut autem", completed: false }
+//FONCTION DE CALL BACK === .then((bruno) => bruno.json())
+//fin de l'adresse de mon fetch j'ajoute ces terminaisons pour accéder à l'API : post,comments,albums,photos,todo,user.
